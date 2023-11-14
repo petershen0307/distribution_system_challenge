f8c7887edb73f0379ddb8a9682cd6769f60c7d30
Peter Shen <peter_shen@trendmicro.com>
2023-11-14 15:38:05 -0500

    add challenge 1 echo

--- challenge_1_echo/main.go
+++ challenge_1_echo/main.go
@@ -1,3 +1,25 @@
 package main
 
 // challenge https://fly.io/dist-sys/1/
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		body := make(map[string]interface{})
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		body["type"] = "echo_ok"
+		return n.Reply(msg, body)
+	})
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/petershen0307/distribution_system_challenge
 
 go 1.20
+
+require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231031014016-1815896cad51
